3ae951b9e0d31445d22d11385ca888711ba69f6f
taa <adornettoluigi98@gmail.com>
2021-06-13 10:39:45 +0200

    version 0.0.2: Added default text styles

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
 ## 0.0.1
 
 * First version with base components
+
+## 0.0.2
+
+* Added default text styles
--- lib/design/text.dart
+++ lib/design/text.dart
@@ -2,6 +2,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
+/// IMPORTING LOCAL PACKAGES
+import 'colors.dart';
+
 /// Define the standard design style for any text
 class TextDesign{
 
@@ -32,5 +35,55 @@ class TextDesign{
       fontWeight: FontWeight.w500,
       fontSize: 0
   );
+
+
+  /// The text style for the selected button text
+  static TextStyle selectedButtonText = TextDesign.defaultBoldTextStyle.apply(
+      fontSizeDelta: 18,
+      color: DesignColors.bluePrimary
+  );
+
+  /// The text style for the unSelected button text
+  static TextStyle unSelectedButtonText = TextDesign.defaultBoldTextStyle.apply(
+      fontSizeDelta: 18,
+      color: DesignColors.greyPrimary
+  );
+
+  /// The text style for the default black title text
+  static TextStyle defaultBlackTextTitle = TextDesign.defaultBoldTextStyle.apply(
+    fontSizeDelta: 18,
+    color: DesignColors.blackPrimary
+  );
+
+  /// The text style for any default black text
+  static TextStyle defaultTextBlack = TextDesign.defaultNormalTextStyle.apply(
+    fontSizeDelta: 15,
+    color: DesignColors.blackPrimary
+  );
+
+  /// The text style for any default pinky text style
+  static TextStyle defaultPinkTitleText = TextDesign.defaultBoldTextStyle.apply(
+    fontSizeDelta: 18,
+    color: DesignColors.pinkyPrimary
+  );
+
+  /// The text style for any default grey text
+  static TextStyle greyTextStyle = TextDesign.defaultNormalTextStyle.apply(
+    color: DesignColors.greyPrimary,
+    fontSizeDelta: 15
+  );
+
+  /// The text style for the blue light color
+  static TextStyle blueLightTextStyle = TextDesign.defaultNormalTextStyle.apply(
+    fontSizeDelta: 15,
+    color: DesignColors.blueTextCard,
+  );
+
+  /// The text style for any white text
+  static TextStyle defaultWhiteTextStyle = TextDesign.defaultNormalTextStyle.apply(
+    fontSizeDelta: 15,
+    color: Colors.white
+  );
+
 
 }
