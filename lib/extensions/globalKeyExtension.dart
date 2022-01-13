d8fc956ae71bf638dccab7184861b08dc2b79aa1
AlessandroMarrche <marchettiale01@gmail.com>
2022-01-13 08:25:13 +0100

    Refactoring ZwapIconsDropDown

---
+++ lib/extensions/globalKeyExtension.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+extension GlobalKeyExtension on GlobalKey {
+  Rect? get globalPaintBounds {
+    final renderObject = currentContext?.findRenderObject();
+    final translation = renderObject?.getTransformTo(null).getTranslation();
+    if (translation != null && renderObject?.paintBounds != null) {
+      final offset = Offset(translation.x, translation.y);
+      return renderObject!.paintBounds.shift(offset);
+    } else {
+      return null;
+    }
+  }
+
+  Offset? get globalOffset {
+    final renderObject = currentContext?.findRenderObject();
+    final translation = renderObject?.getTransformTo(null).getTranslation();
+    if (translation != null && renderObject?.paintBounds != null) return Offset(translation.x, translation.y);
+    return null;
+  }
+}
--- lib/molecules/iconDropDown/zwapIconDropDown.dart
+++ lib/molecules/iconDropDown/zwapIconDropDown.dart
@@ -1,7 +1,9 @@
 /// IMPORTING THIRD PARTY PACKAGES
 import 'package:flutter/material.dart';
 import 'package:taastrap/taastrap.dart';
+import 'package:zwap_design_system/extensions/globalKeyExtension.dart';
 import 'package:zwap_utils/zwap_utils.dart';
+import 'package:collection/collection.dart';
 
 /// IMPORTING LOCAL PACKAGES
 import 'package:zwap_design_system/atoms/atoms.dart';
@@ -73,7 +75,7 @@ class ZwapIconDropDownsDecoration {
         this.hoverColor = ZwapColors.neutral100,
         this.overlayBackgroundColor = ZwapColors.shades0,
         this.itemHoverColor = ZwapColors.neutral100,
-        this.selectedItemColor = ZwapColors.primary700;
+        this.selectedItemColor = ZwapColors.primary100;
 
   ZwapIconDropDownsDecoration copyWith({
     double? height,
@@ -143,7 +145,7 @@ class ZwapIconDropDowns extends StatefulWidget {
 
   ZwapIconDropDowns({Key? key, required this.emojiItems, required this.onSelectCallBack, this.selectedItem, this.decoration = const ZwapIconDropDownsDecoration()}) : super(key: key);
 
-  _ZwapIconDropDownsState createState() => _ZwapIconDropDownsState(this.selectedItem);
+  _ZwapIconDropDownsState createState() => _ZwapIconDropDownsState();
 }
 
 /// The state for this custom widget
@@ -157,14 +159,16 @@ class _ZwapIconDropDownsState extends State<ZwapIconDropDowns> {
   /// The selected item
   String _selectedItem = "";
 
-  /// Is the dropdown open or closed
-  bool _isDropdownOpen = false;
+  @override
+  void initState() {
+    super.initState();
 
-  _ZwapIconDropDownsState(String? preSelectedItem) {
-    if (preSelectedItem != null) {
-      this._selectedItem = preSelectedItem;
+    if (widget.selectedItem != null) {
+      this._selectedItem = widget.selectedItem!;
     }
   }
+
+  void hideOverlay() => this._dropdownOverlay?.remove();
 
   /// It selects the item from the dropdown values
   void _selectItem(String key) {
@@ -185,63 +189,14 @@ class _ZwapIconDropDownsState extends State<ZwapIconDropDowns> {
   /// It gets the selected item
   String get selectedItem => this._selectedItem.trim() != "" ? this._selectedItem : widget.emojiItems.keys.toList().first;
 
-  /// It builds the row item to display inside the emoji dropdown
-  Widget _rowItem(String char, String itemName, bool isDropdownWidget) {
-    return Container(
-      height: 30,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.all(Radius.circular(14)),
-        color: !isDropdownWidget && (itemName == this.selectedItem || itemName == this._hoveredItem) ? ZwapColors.primary100 : ZwapColors.shades0,
-      ),
-      child: Padding(
-        padding: EdgeInsets.all(10),
-        child: Row(
-          children: [
-            Flexible(
-              flex: 0,
-              fit: FlexFit.tight,
-              child: Padding(
-                padding: EdgeInsets.only(right: 3),
-                child: ZwapText(
-                  text: char.emojiChar(),
-                  textColor: ZwapColors.error400,
-                  zwapTextType: ZwapTextType.h3,
-                ),
-              ),
-            ),
-            Flexible(
-              flex: 0,
-              fit: FlexFit.tight,
-              child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: 10),
-                child: ZwapText(
-                  text: itemName,
-                  textColor: ZwapColors.neutral700,
-                  zwapTextType: ZwapTextType.bodyRegular,
-                ),
-              ),
-            ),
-            isDropdownWidget
-                ? Flexible(
-                    flex: 0,
-                    fit: FlexFit.tight,
-                    child: Padding(
-                      padding: EdgeInsets.only(left: 3),
-                      child: Icon(this._isDropdownOpen ? Icons.arrow_drop_up : Icons.arrow_drop_down),
-                    ),
-                  )
-                : Container()
-          ],
-        ),
-      ),
-    );
-  }
-
   void _toggleOverlay() {
-    if (_dropdownOverlay?.mounted ?? false)
+    if (_dropdownOverlay?.mounted ?? false) {
       _dropdownOverlay!.remove();
-    else
+      _dropdownOverlay = null;
+    } else
       Overlay.of(context)?.insert(_dropdownOverlay = _createOverlay());
+
+    setState(() {});
   }
 
   OverlayEntry _createOverlay() {
@@ -251,49 +206,67 @@ class _ZwapIconDropDownsState extends State<ZwapIconDropDowns> {
       builder: (context) {
         return AnimatedPositioned(
           duration: const Duration(milliseconds: 200),
-          top: (_dropdownKey.globalOffset?.dy ?? 0) + (decorations.height ?? 5) + 5,
+          top: (_dropdownKey.globalOffset?.dy ?? 0) + (decorations.height ?? 50) + 5,
           left: (_dropdownKey.globalOffset?.dx ?? 0) - 10,
-          child: Container(
-            width: decorations.width == null ? null : decorations.width! + 20,
-            decoration: BoxDecoration(
-              color: decorations.overlayBackgroundColor,
-              borderRadius: BorderRadius.circular(decorations.overlayBorderRadius),
-            ),
-            padding: decorations.overlayContentPadding,
-            child: Column(
-              children: widget.emojiItems.keys
-                  .toList()
-                  .map((k) => Material(
-                        color: k == this.selectedItem ? decorations.selectedItemColor : decorations.overlayBackgroundColor,
-                        child: InkWell(
-                          onTap: k != this.selectedItem ? () => this._selectItem(k) : null,
-                          borderRadius: BorderRadius.circular(decorations.hoverBorderRadius),
-                          child: Container(
-                            width: double.infinity,
-                            decoration: BoxDecoration(
-                              color: Colors.transparent,
+          child: TweenAnimationBuilder<double>(
+            duration: const Duration(milliseconds: 350),
+            curve: Curves.decelerate,
+            tween: Tween(begin: 0, end: 1),
+            builder: (context, animation, child) => Opacity(
+              opacity: animation,
+              child: Container(
+                width: decorations.width == null ? null : decorations.width! + 20,
+                decoration: BoxDecoration(
+                  color: decorations.overlayBackgroundColor,
+                  borderRadius: BorderRadius.circular(decorations.overlayBorderRadius),
+                ),
+                padding: decorations.overlayContentPadding,
+                child: Column(
+                  children: widget.emojiItems.keys
+                      .toList()
+                      .mapIndexed((i, k) => Container(
+                            color: Colors.transparent,
+                            margin: EdgeInsets.only(bottom: i != (widget.emojiItems.keys.toList().length - 1) ? decorations.itemSpacing : 0),
+                            child: Material(
+                              color: k == this.selectedItem
+                                  ? decorations.selectedItemColor
+                                  : k == _hoveredItem
+                                      ? decorations.itemHoverColor
+                                      : decorations.overlayBackgroundColor,
                               borderRadius: BorderRadius.circular(decorations.hoverBorderRadius),
-                            ),
-                            padding: decorations.insideItemPadding,
-                            child: Row(
-                              children: [
-                                ZwapText(
-                                  text: k.emojiChar(),
-                                  textColor: ZwapColors.error400,
-                                  zwapTextType: ZwapTextType.h3,
+                              child: InkWell(
+                                onTap: k != this.selectedItem ? () => this._selectItem(k) : null,
+                                hoverColor: decorations.itemHoverColor,
+                                onHover: (val) => val ? _hoverItem(k) : _hoverItem(''),
+                                borderRadius: BorderRadius.circular(decorations.hoverBorderRadius),
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    color: Colors.transparent,
+                                    borderRadius: BorderRadius.circular(decorations.hoverBorderRadius),
+                                  ),
+                                  padding: decorations.insideItemPadding,
+                                  child: Row(
+                                    children: [
+                                      ZwapText(
+                                        text: k.emojiChar(),
+                                        textColor: ZwapColors.error400,
+                                        zwapTextType: ZwapTextType.h3,
+                                      ),
+                                      SizedBox(width: 20),
+                                      ZwapText(
+                                        text: widget.emojiItems[k]!,
+                                        textColor: k == this.selectedItem ? decorations.selectedItemTextColor : decorations.itemsTextColor,
+                                        zwapTextType: k == this.selectedItem ? decorations.selectedItemTextStyle : decorations.itemsTextStyle,
+                                      ),
+                                    ],
+                                  ),
                                 ),
-                                SizedBox(width: 20),
-                                ZwapText(
-                                  text: widget.emojiItems[k]!,
-                                  textColor: k == this.selectedItem ? decorations.selectedItemTextColor : decorations.itemsTextColor,
-                                  zwapTextType: k == this.selectedItem ? decorations.selectedItemTextStyle : decorations.itemsTextStyle,
-                                ),
-                              ],
+                              ),
                             ),
-                          ),
-                        ),
-                      ))
-                  .toList(),
+                          ))
+                      .toList(),
+                ),
+              ),
             ),
           ),
         );
@@ -323,48 +296,35 @@ class _ZwapIconDropDownsState extends State<ZwapIconDropDowns> {
           padding: decorations.contentPadding,
           child: Row(
             children: [
-              ZwapText(
-                text: this.selectedItem.emojiChar(),
-                textColor: ZwapColors.error400,
-                zwapTextType: ZwapTextType.h3,
+              AnimatedSwitcher(
+                duration: const Duration(milliseconds: 150),
+                child: Row(
+                  key: ValueKey(this._selectedItem),
+                  children: [
+                    ZwapText(
+                      text: this.selectedItem.emojiChar(),
+                      textColor: ZwapColors.error400,
+                      zwapTextType: ZwapTextType.h3,
+                    ),
+                    SizedBox(width: 20),
+                    ZwapText(
+                      text: widget.emojiItems[this.selectedItem]!,
+                      textColor: ZwapColors.neutral700,
+                      zwapTextType: ZwapTextType.bodyRegular,
+                    ),
+                  ],
+                ),
               ),
-              SizedBox(width: 20),
-              ZwapText(
-                text: widget.emojiItems[this.selectedItem]!,
-                textColor: ZwapColors.neutral700,
-                zwapTextType: ZwapTextType.bodyRegular,
-              ),
-              SizedBox(width: 20),
+              Spacer(),
               AnimatedRotation(
-                turns: this._isDropdownOpen ? 0.5 : 0,
-                duration: const Duration(milliseconds: 300),
-                child: Icon(Icons.arrow_drop_up, color: Color.fromRGBO(50, 50, 50, 1)),
+                turns: (this._dropdownOverlay?.mounted ?? false) ? 0.5 : 0,
+                duration: const Duration(milliseconds: 150),
+                child: Icon(Icons.keyboard_arrow_up, color: Color.fromRGBO(50, 50, 50, 1), key: ValueKey(this._dropdownOverlay != null)),
               ),
             ],
           ),
         ),
       ),
     );
-  }
-}
-
-//TODO: sposta in file a parte
-extension GlobalKeyExtension on GlobalKey {
-  Rect? get globalPaintBounds {
-    final renderObject = currentContext?.findRenderObject();
-    final translation = renderObject?.getTransformTo(null).getTranslation();
-    if (translation != null && renderObject?.paintBounds != null) {
-      final offset = Offset(translation.x, translation.y);
-      return renderObject!.paintBounds.shift(offset);
-    } else {
-      return null;
-    }
-  }
-
-  Offset? get globalOffset {
-    final renderObject = currentContext?.findRenderObject();
-    final translation = renderObject?.getTransformTo(null).getTranslation();
-    if (translation != null && renderObject?.paintBounds != null) return Offset(translation.x, translation.y);
-    return null;
   }
 }
